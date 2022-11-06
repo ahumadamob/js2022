ad5b12e8cb41105f23aa0268a3a52296184791d5
Mario Ahumada <ahumadamob@gmail.com>
2022-11-06 12:20:29 -0300

    Unidad 1 - TP 2 finalizado

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-function muestraPrecio(){
-    var tipoSeguro = document.getElementById("tipoSeguro").value;
-    var alertPrecio = document.getElementById('alertPrecio');
-    var precio;
-    switch(tipoSeguro){
-        case '1':
-            precio = 500;
-            break;
-        case '2':
-            precio = 1000;
-            break;
-        case '3':
-            precio = 1500;
-            break;
-        default:
-            precio = 0;
-            break;
-    }
-    if(precio > 0){
-        alertPrecio.innerHTML = '$ ' + precio;
-        alertPrecio.style.display = '';
+const sumar = (a, b) => {
+    const suma = parseFloat(a) + parseFloat(b)    
+    if(isNaN(suma)){
+        return 'Indique números para ambos valores'
     }else{
-        alertPrecio.style.display = 'none';
+        return suma
     }
-    
+}
+const restar = (a, b) => a - b
+const multiplicar = (a, b) => a * b
+const dividir = (a, b) => {
+    if(b==0){
+        return 'División por cero'
+    }else{
+        return a / b
+    }    
+}
+
+const operacion = (tipo) => {
+    let vala = document.getElementById("elementoA").value
+    let valb = document.getElementById("elementoB").value    
+    var resultado
+    switch(tipo){
+        case '+':
+            resultado = sumar(vala, valb)
+            break
+        case '-':
+            resultado = restar(vala,valb)
+            break
+        case '*':
+            resultado = multiplicar(vala, valb)
+            break
+        case '/':
+            resultado = dividir(vala, valb)
+            break
+    }
+
+    document.getElementById("resultado").value = resultado
+}
+
+const borrarCampos = () => {
+    document.getElementById("elementoA").value = ""
+    document.getElementById("elementoB").value = ""   
+    document.getElementById("resultado").value = "" 
 }
